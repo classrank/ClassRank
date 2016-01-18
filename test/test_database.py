233ddecf76aa91a4abb91bcbec9704452741a9b4
Joshua Morton <joshua.morton@gatech.edu>
2016-01-18 02:09:21 -0500

    Restructured for Testing
     - Moved the testing directory to the root of the project
     - Updated gitignore and coveragerc with some minor changes

--- test/test_database.py
+++ test/test_database.py
@@ -3,7 +3,6 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 from classrank.database.tables import School, Account, Student, Faculty, Base, Course, Section, Rating
 from sqlalchemy import create_engine
-from classrank import database
 
 
 class TestTables(unittest.TestCase):
@@ -12,10 +11,14 @@ class TestTables(unittest.TestCase):
         self.metadata = Base.metadata.create_all(engine)
         self.session = sessionmaker(bind=engine)()
 
-    def test_account(self):
+    def test_account_basic(self):
         # you can add an account
-        self.session.add(Account(username="bob", email_address="me@test.com",
+        self.session.add(Account(username="bob", email_address="m@test.com",
                                  password_hash="secret", password_salt="table"))
         self.session.commit()
         # and it persists
         self.assertEqual("bob", self.session.query(Account).all()[0].username)
+        self.assertEqual("m@test.com", self.session.query(Account).all()[0].email_address)
+        self.assertEqual("secret", self.session.query(Account).all()[0].password_hash)
+        self.assertEqual("table", self.session.query(Account).all()[0].password_salt)
+
