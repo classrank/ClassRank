c01c20100a501c9336a8d8f2de6623752e46bfa6
mmanguno <mmanguno3@gatech.edu>
2016-02-15 04:12:01 -0500

    Hotfix for namespace errors.

--- classrank/handlers/forms.py
+++ classrank/handlers/forms.py
@@ -8,29 +8,29 @@ required_field_msg = 'This field is required.'
 
 
 class LoginForm(Form):
-    email = fields.StringField(u'email', [wtfv.Email(message="Your email address was invalid"),
+    email = StringField(u'email', [wtfv.Email(message="Your email address was invalid"),
                                           wtfv.required(message="Please include an email address"),
                                           wtfv.Length(min=7, max=64, message="Email address must be 7-64 characters")])
-    password = fields.StringField(u'password',
+    password = StringField(u'password',
                                   [wtfv.required("Please enter a password"),
                                    wtfv.Length(min=6, max=128, message="Password must be 6-128 characters")])
 
 
 class RegistrationForm(Form):
-    username = fields.StringField(u'username',
+    username = StringField(u'username',
                                   [wtfv.required(message="Please enter a username"),
                                    wtfv.Length(min=3, max=25, message="Username must be 3-25 characters")])
-    email = fields.StringField(u'email', [wtfv.Email(message="Not a valid email address"),
+    email = StringField(u'email', [wtfv.Email(message="Not a valid email address"),
                                           wtfv.required(message="Please enter an email address"),
                                           wtfv.Length(min=7, max=64, message="Email address must be 7-64 characters")])
-    # TODO: pull from the database's list of schools
-    school = fields.StringField(u'school', [wtfv.required(message="Please enter a school"),
+
+    school = StringField(u'school', [wtfv.required(message="Please enter a school"),
                                             wtfv.Length(min=3, max=15, message="school should be 3-15 characters")])
-    password = fields.StringField(u'password',
+    password = StringField(u'password',
                                   [wtfv.required(message="Please enter a password"),
                                    wtfv.Length(min=6, max=128, message="Password must be 6-128 characters"),
                                    wtfv.EqualTo('password_confirm', message="Passwords did not match")])
-    password_confirm = fields.StringField(u'password_confirm',
+    password_confirm = StringField(u'password_confirm',
                                           [wtfv.required(message="Please confirm your password"),
                                            wtfv.Length(min=6, max=128, message="Password must be 6-128 characters")])
 
