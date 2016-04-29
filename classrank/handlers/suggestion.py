a9697e363e4a1c0c1c910965e5955674f0d76226
Joshua Morton <joshua.morton@gatech.edu>
2016-04-29 16:51:41 -0400

    Fixed an error with students courses not cascading

--- classrank/handlers/suggestion.py
+++ classrank/handlers/suggestion.py
@@ -13,7 +13,7 @@ class SuggestionHandler(BaseHandler):
             filters = self.add_filters()
             with Query(self.db) as q:
                 student = q.query(self.db.account).filter_by(username=user).one().student
-                courses = set(c[0] for c in q.query(self.db.course.name).all()) - set(student.courses)
+                courses = set(c[0] for c in q.query(self.db.course.name).all()) - set([c.name for c in student.courses])
                 student_id = student.uid
             page_data['data']['rating'] = filters['rating'].getRecommendation({student_id: courses})
 
@@ -27,4 +27,6 @@ class SuggestionHandler(BaseHandler):
         # attrs = set(x for x in dir(self.db.rating) if not x.startswith("_"))
         # attrs -= set(["metadata", "section_id", "student", "section", "student_id"])
         # return {attr: CollaborativeFilter(db=self.db, metric=attr) for attr in attrs}
-        return {'rating': CollaborativeFilter(db=self.db, metric='rating')}
+        with Query(self.db)as q:
+            num = max(1, int(q.query(self.db.user).count() / 5))
+        return {'rating': CollaborativeFilter(db=self.db, metric='rating', numRecommendations=num)}
